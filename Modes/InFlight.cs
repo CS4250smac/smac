83c9b82bd42f37b12467366e8ccb60832f9414c4
raddack <raddack@gmail.com>
2015-10-27 20:26:05 -0600

    Created folder for modes. Add barebones InFlight.cs

---
+++ Modes/InFlight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace InFlightMode
+{
+    public class InFlight
+    {
+        public Double CollisionCheck() {
+            return 0;
+        }
+        public int WarningMessage(double CollisionDistance)
+        {
+            return 0;
+        }
+        public int ActionRequestMessage(int warningMessage)
+        {
+            return 0;
+        }
+    }
+}
