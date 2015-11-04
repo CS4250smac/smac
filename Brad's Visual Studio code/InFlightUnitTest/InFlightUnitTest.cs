2d01d44c573ee53b6e340d8e5259c4b695cfd0cb
raddack <raddack@gmail.com>
2015-11-04 14:38:39 -0700

    ADD my Visual Studio Code

---
+++ Brad's Visual Studio code/InFlightUnitTest/InFlightUnitTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using InFlightMode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InFlightUnitTest
+{
+    [TestClass]
+    public class InFlightUnitTest
+    {
+        private byte[] AdvanceTime(byte[] oldPOS)
+        {
+            byte[] newPOS;
+            // move the postion based on vector;
+            newPOS = oldPOS;
+            return newPOS;
+        }
+        // Test 1: A1 is on opposite course of A0.
+        // Validation: no warning message, plan should be tracked till out of range.
+        [TestMethod]
+        public void TestMethod1(byte[] A0, byte[] A1)
+        {
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                Assert.AreEqual(expected, actual);
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
+
+        }
+
+        // Test 2: A1 is on course that will pass by A0 within X (1st warning area) distance of each other.
+        // Validation: no warning message, First warning message, no warning message.
+        [TestMethod]
+        public void TestMethod2(byte[] A0, byte[] A1)
+        {
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5)
+                {
+                    expected++;
+                }
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
+        }
+
+        // Test 3: A1 is on course that will pass by A0 within X (2nd warning area) distance of each other.
+        // Validation: no warning message, First warning message, second warning message no warning message.
+        [TestMethod]
+        public void TestMethod3(byte[] A0, byte[] A1)
+        {
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10)
+                {
+                    expected++;
+                }
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
+        }
+
+        // Test 4: A1 is on course that will pass by A0 within X (3rd warning area) distance of each other.
+        // Validation: no warning message, First warning message, Second warning message, Third warning message, no warning message.
+       [TestMethod]
+        public void TestMethod4(byte[] A0, byte[] A1)
+        {
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10 || t == 15)
+                {
+                    expected++;
+                }
+                if (t == 20)
+                {
+                    expected = 0;
+                }
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
+        }
+
+        // Test 5: A1 is on course that will pass by A0 within 0 distance and collision will occur.
+        // Validation:  no warning message, First warning message, Second warning message, Third warning message.
+        [TestMethod]
+        public void TestMethod5(byte[] A0, byte[] A1)
+        {
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10 || t == 15)
+                {
+                    expected++;
+                }
+
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
+        }
+    }
+}
---
+++ Brad's Visual Studio code/InFlightUnitTest/InFlightUnitTestStatic.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using InFlightMode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*
+* @author team SMAC
+*
+* This is static Method test for InFlight mode
+* 
+* ToDo: Fix input, output to be agreed upon.
+*/
+namespace InFlightUnitTest
+{
+    [TestClass]
+    public class InFlightUnitTestStatic
+    {
+        [TestMethod]
+        public void CollisionCheckMethodTest()
+        {
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte [1];
+            double expectedDistance = 1;
+            InFlight inFlight = new InFlight();
+            double actualDistance = inFlight.CollisionCheck(A0,A1);
+            Assert.AreEqual(actualDistance, expectedDistance);
+        }
+        [TestMethod]
+        public void NoWarningMessageTest()
+        {
+            int expectedWarning = 0;
+            double collisionDistance = 400;
+
+            InFlight inFlight = new InFlight();
+            int actualWarning = inFlight.WarningMessage(collisionDistance);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+        [TestMethod]
+        public void FirstWarningMessageTest()
+        {
+            int expectedWarning = 1;
+            double collisionDistance = 200;
+
+            InFlight inFlight = new InFlight();
+            int actualWarning = inFlight.WarningMessage(collisionDistance);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+         }
+        [TestMethod]
+        public void SecondWarningMessageTest()
+        {
+
+            int expectedWarning = 2;
+            double collisionDistance = 100;
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.WarningMessage(collisionDistance);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+
+        [TestMethod]
+        public void ThirdWarningMessageTest()
+        {
+            int expectedWarning = 3;
+            double collisionDistance = 25;
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.WarningMessage(collisionDistance);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+
+
+        [TestMethod]
+        public void BadDataWarningMessageTest()
+        {
+            double collisionDistance = -25;
+            InFlight inFlight = new InFlight();
+            try {
+                int actualWarning = inFlight.WarningMessage(collisionDistance);
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Bad data");
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        [TestMethod]
+        public void NoActionRequestMessageTest()
+        {
+            int expectedWarning= 0;
+            int warningLevel = 2;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.ActionRequestMessage(warningLevel, A0,A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+            inFlight = new InFlight();
+            warningLevel = 3;
+            actualWarning = inFlight.ActionRequestMessage(warningLevel, A0,A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+        [TestMethod]
+        public void UpActionRequestMessageTest()
+        {
+            int expectedWarning = 1;
+            int warningLevel = 2;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+            inFlight = new InFlight();
+            warningLevel = 3;
+            actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+        [TestMethod]
+        public void DownActionRequestMessageTest()
+        {
+
+            int expectedWarning = 2;
+            int warningLevel = 2;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+            inFlight = new InFlight();
+            warningLevel = 3;
+            actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+        }
+
+        [TestMethod]
+        public void LeftActionRequestMessageTest()
+        {
+            int expectedWarning = 3;
+            int warningLevel = 2;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+            inFlight = new InFlight();
+            warningLevel = 3;
+            actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+        [TestMethod]
+        public void RightActionRequestMessageTest()
+        {
+            int expectedWarning = 4;
+            int warningLevel = 2;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+
+            InFlight inFlight = new InFlight();
+
+            int actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+
+            inFlight = new InFlight();
+            warningLevel = 3;
+            actualWarning = inFlight.ActionRequestMessage(warningLevel, A0, A1);
+            Assert.AreEqual(actualWarning, expectedWarning);
+        }
+
+        [TestMethod]
+        public void BadInputActionRequestMessageTest()
+        {
+            int badWarning = -25;
+            byte[] A0 = new byte[1];
+            byte[] A1 = new byte[1];
+            InFlight inFlight = new InFlight();
+            try
+            {
+                int actualWarning = inFlight.ActionRequestMessage(badWarning,A0,A1);
+                   }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Bad data");
+                Console.Write(e);
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+
+
+
+
+
+
+    }
+}
