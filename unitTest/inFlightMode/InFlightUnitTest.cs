b7f3c71dd77d5aa8cecee20a3c5b7513cc1fe09e
raddack <raddack@gmail.com>
2015-11-04 14:02:30 -0700

    Update unfinished methods.

--- unitTest/inFlightMode/InFlightUnitTest.cs
+++ unitTest/inFlightMode/InFlightUnitTest.cs
@@ -1,129 +1,146 @@
 ﻿using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InFlightMode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace InFlightUnitTest
 {
-   // [TestClass]
+    [TestClass]
     public class InFlightUnitTest
     {
-
+        private byte[] AdvanceTime(byte[] oldPOS)
+        {
+            byte[] newPOS;
+            // move the postion based on vector;
+            newPOS = oldPOS;
+            return newPOS;
+        }
         // Test 1: A1 is on opposite course of A0.
         // Validation: no warning message, plan should be tracked till out of range.
-      //  [TestMethod]
-        public void TestMethod1()
+        [TestMethod]
+        public void TestMethod1(byte[] A0, byte[] A1)
         {
-            double expected = 1;
+            double expected = 0;
+            double actual;
+
+
+            InFlight inFlight = new InFlight();
             //int t, is time in this case. 
             for (int t = 0; t >= 30; t++)
             {
-                //call inflightmethod.
-                //InFlight.CollisionCheck();
-                double actual = 1; // = inflightmethod
-
-               // Assert.AreEqual(expected, actual); 
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                Assert.AreEqual(expected, actual);
+                AdvanceTime(A0);
+                AdvanceTime(A1);
             }
-       
-            
-
 
         }
 
         // Test 2: A1 is on course that will pass by A0 within X (1st warning area) distance of each other.
         // Validation: no warning message, First warning message, no warning message.
-   //     [TestMethod]
-        public void TestMethod2()
+        [TestMethod]
+        public void TestMethod2(byte[] A0, byte[] A1)
         {
+            double expected = 0;
+            double actual;
 
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5)
+                {
+                    expected++;
+                }
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
         }
 
         // Test 3: A1 is on course that will pass by A0 within X (2nd warning area) distance of each other.
         // Validation: no warning message, First warning message, second warning message no warning message.
- //       [TestMethod]
-        public void TestMethod3()
+        [TestMethod]
+        public void TestMethod3(byte[] A0, byte[] A1)
         {
+            double expected = 0;
+            double actual;
 
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10)
+                {
+                    expected++;
+                }
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
         }
 
         // Test 4: A1 is on course that will pass by A0 within X (3rd warning area) distance of each other.
         // Validation: no warning message, First warning message, Second warning message, Third warning message, no warning message.
-  //      [TestMethod]
-        public void TestMethod4()
+        [TestMethod]
+        public void TestMethod4(byte[] A0, byte[] A1)
         {
+            double expected = 0;
+            double actual;
 
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10 || t == 15)
+                {
+                    expected++;
+                }
+                if (t == 20)
+                {
+                    expected = 0;
+                }
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
         }
 
         // Test 5: A1 is on course that will pass by A0 within 0 distance and collision will occur.
         // Validation:  no warning message, First warning message, Second warning message, Third warning message.
-  //      [TestMethod]
-        public void TestMethod5()
+        [TestMethod]
+        public void TestMethod5(byte[] A0, byte[] A1)
         {
+            double expected = 0;
+            double actual;
 
+
+            InFlight inFlight = new InFlight();
+            //int t, is time in this case. 
+            for (int t = 0; t >= 30; t++)
+            {
+                actual = inFlight.WarningMessage(inFlight.CollisionCheck(A0, A1));
+                if (t == 5 || t == 10 || t == 15)
+                {
+                    expected++;
+                }
+
+
+                Assert.AreEqual(expected, actual);
+
+                AdvanceTime(A0);
+                AdvanceTime(A1);
+            }
         }
-
-        // Test 6: A1 is  will be pass infront A0 but will not be 1st warning area range.
-        // Validation: no warning message.
-  //      [TestMethod]
-        public void TestMethod6()
-        {
-
-        }
-
-        // Test 7: A1 is  will be pass infront A0 within 1st warning area range.
-        // Validation: no warning message, 1st warning message, no warning message.
-  //      [TestMethod]
-        public void TestMethod7()
-        {
-
-        }
-
-        // Test 8: A1 is  will be pass infront A0 within  2nd warning area range.
-        // Validation: no warning message, 1st warning message, 2nd warning message, no warning message.
-  //      [TestMethod]
-        public void TestMethod8()
-        {
-
-        }
-
-        // Test 9: A1  is not moving. A0 is on course that will make A1 0 distance apart and collision will occur.
-        // Validation:  no warning message, First warning message, Second warning message, Third warning message.
- //       [TestMethod]
-        public void TestMethod9()
-        {
-
-        }
-
-        // Test 10: A0  is not moving. A1 is on course that will make A0 0 distance apart and collision will occur.
-        // Validation:  no warning message, First warning message, Second warning message, Third warning message.
-//        [TestMethod]
-        public void TestMethod10()
-        {
-
-        }
-
-        // Test 11: A1 is  will be pass behind A0 but will not be 1st warning area range.
-        // Validation: no warning message.
-//        [TestMethod]
-        public void TestMethod11()
-        {
-
-        }
-
-        // Test 12: A1 is  will be pass behind A0 within 1st warning area range.
-        // Validation: no warning message, 1st warning message, no warning message.
-//        [TestMethod]
-        public void TestMethod12()
-        {
-
-        }
-
-        // Test 13: A1 is  will be pass behind A0 within  2nd warning area range.
-        // Validation: no warning message, 1st warning message, 2nd warning message, no warning message.
- //       [TestMethod]
-        public void TestMethod13()
-        {
-
-        }
-
-
     }
 }
