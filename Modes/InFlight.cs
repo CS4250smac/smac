f99c51b1cbbdeed291d7bdb2d6076cf45edb7e13
Matt McGuire <matthew.mcguire3@gmail.com>
2015-11-08 18:12:01 -0700

    NOT COMPILING

--- Modes/InFlight.cs
+++ Modes/InFlight.cs
@@ -46,14 +46,23 @@ namespace InFlightMode
 
         public decimal[] getData(string table, string col)
         {
-        	
+
         }
 
         public static void main(string[] args) {
-        	decimal[] safeLat1 = getData("safe_data", "lastitude_1");
+        	string safeTable = "safe_data", dangerTable = "collision_data", lat1 = "latitude_1", long1 = "longitude_1", lat2 = "latitude_2", long2 = "longitude_2";
+
+        	decimal[] safeLat1 = getData(safeTable, lat1);
+        	decimal[] safeLong1 = getData(safeTable, long1);
+        	decimal[] safeLat2 = getData(safeTable, lat2);
+        	decimal[] safeLong2 = getData(safeTable, long2);
         	int safeMessage = ActionRequestMessage(WarningMessage(CollisionDistance(safeLat1, safeLong1, safeLat2, safeLong2)));
         	Debug.Assert(safeMessage == 0, "Wrong action request message, expecting 0 but was: " + safeMessage);
 
+        	decimal[] dangerLat1 = getData(dangerTable, lat1);
+        	decimal[] dangerLong1 = getData(dangerTable, long1);
+        	decimal[] dangerLat2 = getData(safedangerTableTable, lat2);
+        	decimal[] dangerLong2 = getData(safedangerTableTable, long2);
         	int collisionMessage = ActionRequestMessage(WarningMessage(CollisionDistance(dangerLat1, dangerLong1, dangerLat2, dangerLong2)));
         	Debug.Assert(collisionMessage == 1, "Wrong action request message, expecting 1 but was: " + collisionMessage);
         }
