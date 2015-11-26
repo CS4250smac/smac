d23f275a72e2470d8c6c5008d3f6b2d6dd163186
Matt McGuire <matthew.mcguire3@gmail.com>
2015-11-26 14:02:30 -0700

    Final data for static tests

--- data/DataGenerator.cs
+++ data/DataGenerator.cs
@@ -8,8 +8,8 @@ namespace DataGenerator
 {
 	class DataGenerator
 	{
-		static Dictionary<string,List<Decimal>> makeValues(decimal latVal, decimal lonVal, decimal latValTwo,
-			decimal lonValTwo, int operation, int currentIteration, int maxIteration, string table,
+		static Dictionary<string,List<Decimal>> makeValues(decimal latVal, decimal lonVal, decimal altOne, decimal latValTwo,
+			decimal lonValTwo, decimal altTwo, int scenario, int currentIteration, int maxIteration, string table,
 			Dictionary<string, List<Decimal>> values)
 		{
 			if (currentIteration == maxIteration)
@@ -18,42 +18,68 @@ namespace DataGenerator
 			}
 			else
 			{
-				switch(operation)
+				switch(scenario)
 				{
 					case 0:
-						latVal = latVal + (latVal * new Decimal(.003));
-						latValTwo = latValTwo + (latValTwo * new Decimal(.003));
-						lonVal = lonVal - (lonVal * new Decimal(.003));
-						lonValTwo = lonValTwo - (lonValTwo * new Decimal(.003));
-						break;
+					latVal = latVal + (latVal * new Decimal(.003));
+					latValTwo = latValTwo + (latValTwo * new Decimal(.003));
+					lonVal = lonVal - (lonVal * new Decimal(.003));
+					lonValTwo = lonValTwo - (lonValTwo * new Decimal(.003));
+					break;
 					case 1:
-						latVal = latVal + (latVal * new Decimal(.003));
-						latValTwo = latValTwo + (latValTwo * new Decimal(.003));
-						lonVal = lonVal + (lonVal * new Decimal(.003));
-						lonValTwo = lonValTwo + (lonValTwo * new Decimal(.003));
-						break;
+					latVal = latVal + (latVal * new Decimal(.003));
+					latValTwo = latValTwo + (latValTwo * new Decimal(.003));
+					lonVal = lonVal + (lonVal * new Decimal(.003));
+					lonValTwo = lonValTwo + (lonValTwo * new Decimal(.003));
+					altOne = altOne + 250;
+					break;
+					case 2:
+					latVal = latVal + (latVal * new Decimal(.003));
+					latValTwo = latValTwo + (latValTwo * new Decimal(.003));
+					lonVal = lonVal - (lonVal * new Decimal(.003));
+					lonValTwo = lonValTwo - (lonValTwo * new Decimal(.003));
+					altOne = altOne + 250;
+					break;
+					case 3:
+					latVal = latVal + (latVal * new Decimal(.003));
+					latValTwo = latValTwo + (latValTwo * new Decimal(.003));
+					lonVal = lonVal + (lonVal * new Decimal(.003));
+					lonValTwo = lonValTwo + (lonValTwo * new Decimal(.003));
+					break;
 					default:
-						break;
+					break;
 				}
 
-				values.Add(table, new List<Decimal>(new decimal[] {latVal, lonVal, latValTwo, lonValTwo}));
-				return makeValues(latVal, lonVal, latValTwo, lonValTwo, operation,
+				values.Add(table, new List<Decimal>(new decimal[] {latVal, lonVal, altOne, latValTwo, lonValTwo, altTwo}));
+				return makeValues(latVal, lonVal, altOne, latValTwo, lonValTwo, altTwo, scenario,
 					++currentIteration, maxIteration, table + currentIteration, values);
 			}
 		}
 
 		static void collectData(IDbConnection dbConnection)
 		{
-			//Crash table
-			Dictionary<string, List<Decimal>> crashVals = makeValues(new Decimal(39.73929),
-				new Decimal(104.99031), new Decimal(40.73929), new Decimal(105.99031), 0, 0, 30, 
-				"collision_data", new Dictionary<string, List<Decimal>>());
-			writeData(dbConnection, crashVals);
+			//Vector crash table
+			Dictionary<string, List<Decimal>> vectorCrashVals = makeValues(new Decimal(39.73929),
+				new Decimal(104.99031), new Decimal(10000), new Decimal(40.73929), new Decimal(105.99031),
+				new Decimal(15000), 0, 0, 30, "vector", new Dictionary<string, List<Decimal>>());
+			writeData(dbConnection, vectorCrashVals);
+
+			//Altitude crash table
+			Dictionary<string, List<Decimal>> altCrashVals = makeValues(new Decimal(-39.73929),
+				new Decimal(-104.99031), new Decimal (10000), new Decimal(40.73929), new Decimal(105.99031),
+				new Decimal(15000), 1, 0, 30, "altitude", new Dictionary<string, List<Decimal>>());
+			writeData(dbConnection, altCrashVals);
+
+			//Both altitude and vector crash table
+			Dictionary<string, List<Decimal>> altVectCrashVals = makeValues(new Decimal(39.73929),
+				new Decimal(104.99031), new Decimal(10000), new Decimal(40.73929), new Decimal(105.99031),
+				new Decimal(15000), 2, 0, 30, "alt_vct", new Dictionary<string, List<Decimal>>());
+			writeData(dbConnection, altVectCrashVals);
 
 			//Safe table
 			Dictionary<string, List<Decimal>> safeVals = makeValues(new Decimal(-39.73929),
-				new Decimal(-104.99031), new Decimal(40.73929), new Decimal(105.99031), 1, 0, 30,
-				"safe_data", new Dictionary<string, List<Decimal>>());
+				new Decimal(-104.99031), new Decimal(10000), new Decimal(40.73929), new Decimal(105.99031),
+				new Decimal(15000), 3, 0, 30, "safe_data", new Dictionary<string, List<Decimal>>());
 			writeData(dbConnection, safeVals);
 		}
 
@@ -71,15 +97,33 @@ namespace DataGenerator
 				List<Decimal> valsList = values[key];
 				decimal lat = valsList[0];
 				decimal lon = valsList[1];
-				decimal latTwo = valsList[2];
-				decimal lonTwo = valsList[3];
+				decimal altOne = valsList[2];
+				decimal latTwo = valsList[3];
+				decimal lonTwo = valsList[4];
+				decimal altTwo = valsList[5];
 
 
-				string table = key.StartsWith("collision") ? "collision_data" : "safe_data";
-				
+				string table = null;
+				if (key.StartsWith("altitude"))
+				{
+					table = "collision_altitude_data";
+				}
+				else if (key.StartsWith("vector"))
+				{
+					table = "collision_vector_data";
+				}
+				else if (key.StartsWith("alt_vct"))
+				{
+					table = "collision_altitude_vector_data";
+				}
+				else
+				{
+					table = "safe_data";
+				}
+
 				dbCommands.CommandText = String.Format("INSERT INTO {0}" +
-					"(`id`, latitude_1, longitude_1, latitude_2, longitude_2)" +
-					"VALUES (?ts, ?la_1, ?lo_1, ?la_2, ?lo_2);", table);
+					"(`id`, latitude_1, longitude_1, altitude_1, latitude_2, longitude_2, altitude_2)" +
+					"VALUES (?ts, ?la_1, ?lo_1, ?alt_1, ?la_2, ?lo_2, ?alt_2);", table);
 
 				var parameterTimeStamp = dbCommands.CreateParameter();
 				parameterTimeStamp.ParameterName = "?ts";
@@ -99,6 +143,12 @@ namespace DataGenerator
 				parameterLongitude.DbType = DbType.Decimal;
 				dbCommands.Parameters.Add(parameterLongitude);
 
+				var parameterAltitudeOne = dbCommands.CreateParameter();
+				parameterAltitudeOne.ParameterName = "?alt_1";
+				parameterAltitudeOne.Value = Decimal.ToInt32(altOne);
+				parameterAltitudeOne.DbType = DbType.Int32;
+				dbCommands.Parameters.Add(parameterAltitudeOne);
+
 				var parameterLatitudeTwo = dbCommands.CreateParameter();
 				parameterLatitudeTwo.ParameterName = "?la_2";
 				parameterLatitudeTwo.Value = latTwo;
@@ -111,15 +161,21 @@ namespace DataGenerator
 				parameterLongitudeTwo.DbType = DbType.Decimal;
 				dbCommands.Parameters.Add(parameterLongitudeTwo);
 
+				var parameterAltitudeTwo = dbCommands.CreateParameter();
+				parameterAltitudeTwo.ParameterName = "?alt_2";
+				parameterAltitudeTwo.Value = Decimal.ToInt32(altTwo);
+				parameterAltitudeTwo.DbType = DbType.Int32;
+				dbCommands.Parameters.Add(parameterAltitudeTwo);
+
 				dbCommands.ExecuteNonQuery();
 
 				Random rnd = new Random();
-				int randomSleep = rnd.Next(500, 2000);
+				int randomSleep = rnd.Next(500, 1000);
 				Thread.Sleep(randomSleep);
 
 				dbCommands.Parameters.Clear();
 			}
-			
+
 			dbCommands.Dispose();
 			dbCommands = null;
 		}
@@ -141,7 +197,7 @@ namespace DataGenerator
 		{
 			IDbConnection dbConnection = connectDb();
 			collectData(dbConnection);
-			
+
 			//Close down the DB connection
 			dbConnection.Close();
 			dbConnection = null;
